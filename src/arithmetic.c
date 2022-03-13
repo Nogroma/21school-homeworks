e1543c110998047e9cb1074ce67bed870d30c1a4
Nogroma <nogroma@gmail.com>
2022-03-13 16:52:15 +0300

    ✨ Complete quest 3

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,12 +1,51 @@
+// Copyright 2021, Nogroma. All rights reserved.
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-int sum(int a, int b);
-
-int main()
-{
-    return 0;
+bool isInteger(double d) {
+    return d == (int64_t)d;
 }
 
-int sum(int a, int b){
-    return (2 * a * b) / (a - b);
+int division(int a, int b) {
+    return a / b;
+}
+
+int multiplication(int a, int b) {
+    return a * b;
+}
+
+int subtraction(int a, int b) {
+    return a - b;
+}
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    float first_number_dirty, second_number_dirty;
+
+    scanf("%f %f", &first_number_dirty, &second_number_dirty);
+
+    if (!isInteger(first_number_dirty) || !isInteger(second_number_dirty)) {
+        return 1;
+    }
+
+    int first_number = first_number_dirty;
+    int second_number = second_number_dirty;
+
+    printf(
+        "%d, %d, %d ",
+        sum(first_number, second_number),
+        subtraction(first_number, second_number),
+        multiplication(first_number, second_number));
+
+    if (second_number == 0) {
+        printf("n/a");
+    } else {
+        printf("%d", division(first_number, second_number));
+    }
+
+    return 0;
 }
