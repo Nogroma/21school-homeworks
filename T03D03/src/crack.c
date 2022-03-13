f4193f056f800a12c04ece4a7e280cdacba3be95
Nogroma <nogroma@gmail.com>
2022-03-13 19:03:23 +0300

    ✨ Complete quest 7

---
+++ T03D03/src/crack.c
@@ -0,0 +1,20 @@
+// Copyright 2021, Nogroma. All rights reserved.
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x, y;
+
+    if (scanf("%lf %lf", &x, &y) != 2) {
+        printf("n/a");
+        return 1;
+    }
+
+    if ((x * x + y * y) <= 25) {
+        printf("GOTCHA");
+    } else {
+        printf("MISS");
+    }
+
+    return 0;
+}
