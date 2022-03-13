aa323decbd8e00e584b0fdea7d1871e4e65c4631
Nogroma <nogroma@gmail.com>
2022-03-13 18:33:09 +0300

    ✨ Complete quest 5

---
+++ T03D03/src/important_function.c
@@ -0,0 +1,22 @@
+// Copyright 2021, Nogroma. All rights reserved.
+#include <stdio.h>
+#include <math.h>
+
+float magic_function(float x) {
+    return 7e-3 * pow(x, 4) + ((22.8 * pow(x, 1 / 3) - 1e3) * x + 3) / (x * x / 2) - x * pow(10 + x, 2 / x) - 1.01;
+}
+
+int main() {
+    float x;
+
+    if (scanf("%f", &x) != 1)
+    {
+        printf("n/a");
+
+        return 1;
+    }
+
+    printf("%.1lf", magic_function(x));
+
+    return 0;
+}
