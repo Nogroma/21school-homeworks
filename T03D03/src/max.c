e7ae3da868bdf718fb609007aac762077517f926
Nogroma <nogroma@gmail.com>
2022-03-13 17:59:54 +0300

    ✨ Complete quest 4

---
+++ T03D03/src/max.c
@@ -0,0 +1,24 @@
+// Copyright 2021, Nogroma. All rights reserved.
+#include <stdio.h>
+
+int max(int a, int b) {
+    if (a > b) {
+        return a;
+    }
+
+    return b;
+}
+
+int main() {
+    int first_number, second_number;
+
+    if (scanf("%d %d", &first_number, &second_number) != 2) {
+        printf("n/a");
+
+        return 1;
+    }
+
+    printf("%d", max(first_number, second_number));
+
+    return 0;
+}
