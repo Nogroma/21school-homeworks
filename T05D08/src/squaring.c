88c77667da4ee8f1742f42086c0b78795f9004cd
Nogroma <nogroma@gmail.com>
2022-03-17 16:51:05 +0300

    ✨ Complete T05D08 quest 2

--- T05D08/src/squaring.c
+++ T05D08/src/squaring.c
@@ -5,33 +5,45 @@ int input(int *a, int *n);
 void output(int *a, int n);
 void squaring(int *a, int n);
 
-int main()
-{
+int input(int *a, int *n) {
+    printf("Введите количество ячеек массива\n");
+
+    if (scanf("%d", n) != 1 || *n > NMAX || *n <= 0) {
+        return 1;
+    };
+
+    for (int *p = a; p - a < *n; p++) {
+        printf("Введите элемент массива: ");
+        scanf("%d", p);
+    }
+
+    return 0;
+}
+
+void output(int *a, int n) {
+    for (int *p = a; p - a < n; p++) {
+        printf("> %d\n", *p);
+    }
+}
+
+void squaring(int *a, int n) {
+    for (int *p = a; p - a < n; p++) {
+        *p = *p * *p;
+    }
+}
+
+int main() {
     int n, data[NMAX];
-    input(data, n);
+
+    int input_code = input(data, &n);
+    if (input_code != 0) {
+        printf("n/a");
+
+        return 1;
+    }
+
     squaring(data, n);
     output(data, n);
 
     return 0;
 }
-
-int input(int *a, int *n)
-{
-    scanf("%d", n);
-    for(int *p = a; p - a < *n; p++)
-    {
-        scanf("%d", p);
-    }
-}
-
-void output(int *a, int n)
-{
-    //NOTHING
-}
-
-void squaring(int *a, int n)
-{
-    //NOTHING
-}
-
-
