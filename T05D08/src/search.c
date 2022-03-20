44d4ddd175a5cfc394bfd72494a7f927566ce8a7
Nogroma <nogroma@gmail.com>
2022-03-20 15:25:57 +0300

    ✨ Comlete T05D08 quests 3 and 4

--- T05D08/src/search.c
+++ T05D08/src/search.c
@@ -1,16 +1,83 @@
-/*
-    Search module for the desired value from data array.
+// Copyright 2022, Nogroma. All rights reserved.
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-    Returned value must be:
-        - "even"
-        - ">= mean"
-        - "<= mean + 3 * sqrt(variance)"
-        - "!= 0"
+#define NMAX 30
 
-        OR
+bool isOdd(int num) {
+    return num % 2 == 0;
+}
 
-        0
-*/
+int input(int *a, int *n) {
+    printf("Введите количество ячеек массива\n");
 
+    if (scanf("%d", n) != 1 || *n > NMAX || *n <= 0) {
+        return 1;
+    }
 
+    for (int *p = a; p - a < *n; p++) {
+        printf("Введите элемент массива: ");
+        scanf("%d", p);
+    }
 
+    return 0;
+}
+
+int max(int *a, int n) {
+    int max_value = *a;
+
+    for (int *p = a; p - a < n; p++) {
+        if (*p > max_value) {
+            max_value = *p;
+        }
+    }
+
+    return max_value;
+}
+
+int min(int *a, int n) {
+    int min_value = *a;
+
+    for (int *p = a; p - a < n; p++) {
+        if (*p < min_value) {
+            min_value = *p;
+        }
+    }
+
+    return min_value;
+}
+
+double mean(int *a, int n) {
+    return (max(a, n) + min(a, n)) / 2;
+}
+
+double variance(int *a, int n) {
+    return mean(a, n) / 2;
+}
+
+double get_result(int *a, int n) {
+    double result = max(a, n);
+
+    if (isOdd(result) ||
+        result >= mean(a, n) ||
+        result <= mean(a, n) + 3 * sqrt(variance(a, n)) ||
+        result != 0) {
+        return result;
+    }
+
+    return 0;
+}
+
+void output(int result_v) {
+    printf("%d", result_v);
+}
+
+int main() {
+    int n, data[NMAX];
+
+    input(data, &n);
+    output(get_result(data, n));
+
+    return 0;
+}
--- T05D08/src/stat.c
+++ T05D08/src/stat.c
@@ -13,11 +13,68 @@ void output_result(int max_v,
                    double mean_v,
                    double variance_v);
 
-int main()
-{
+int input(int *a, int *n) {
+    printf("Введите количество ячеек массива\n");
+
+    if (scanf("%d", n) != 1 || *n > NMAX || *n <= 0) {
+        return 1;
+    };
+
+    for (int *p = a; p - a < *n; p++) {
+        printf("Введите элемент массива: ");
+        scanf("%d", p);
+    }
+
+    return 0;
+}
+
+int max(int *a, int n) {
+    int max_value = *a;
+    for (int *p = a; p - a < n; p++) {
+        if (*p > max_value) {
+            max_value = *p;
+        }
+    }
+    return max_value;
+}
+
+int min(int *a, int n) {
+    int min_value = *a;
+    for (int *p = a; p - a < n; p++) {
+        if (*p < min_value) {
+            min_value = *p;
+        }
+    }
+    return min_value;
+}
+
+double mean(int *a, int n) {
+    return (double)(max(a, n) + min(a, n)) / 2;
+}
+
+double variance(int *a, int n) {
+    return mean(a, n) / 2;
+}
+
+void output(int *a, int n) {
+    for (int *p = a; p - a < n; p++) {
+        printf("%d ", *p);
+    }
+
+    printf("\n");
+}
+
+void output_result(int max_v, int min_v, double mean_v, double variance_v) {
+    printf("%d %d %lf %lf", max_v, min_v, mean_v, variance_v);
+}
+
+int main() {
     int n, data[NMAX];
-    input(data, n);
+
+    input(data, &n);
+
     output(data, n);
+
     output_result(max(data, n),
                   min(data, n),
                   mean(data, n),
@@ -25,5 +82,3 @@ int main()
 
     return 0;
 }
-
-
